836422a837fe9c4cb33c9dcdedd4d19a2b9a5bd8
enzo <enzoserafin@gmail.com>
2021-05-12 12:04:20 -0300

    taskScreen

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { SafeAreaView, StatusBar } from 'react-native';
-import Home from './src/views/Home';
+// import Home from '°./src/views/Home';
+import Task from './src/views/Task';
 
 export default function App() {
   return (
     <>
-      <SafeAreaView backgroundColor="#20295F">
+      <SafeAreaView backgrounßdColor="#20295F">
         <StatusBar barStyle="light-content" backgroundColor="#20295F" />
       </SafeAreaView>
-      <Home />
+      <Task />
     </>
   );
 }
---
+++ src/views/Task/index.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import {
+  View,
+  ScrollView,
+  Image,
+  Text,
+  TextInput,
+  KeyboardAvoidingView,
+  TouchableOpacity,
+  Switch,
+} from 'react-native';
+
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+import typeIcons from '../../utils/typeIcons';
+import styles from './styles';
+
+export default function Task() {
+  const [done, setDone] = useState(false);
+  return (
+    <KeyboardAvoidingView behavior="padding" style={styles.container}>
+      <Header showBack />
+      <ScrollView style={{ width: '100%' }}>
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          style={{ marginVertical: 10 }}
+        >
+          {typeIcons.map(
+            icon =>
+              icon != null && (
+                <TouchableOpacity>
+                  <Image source={icon} style={styles.imageIcon} />
+                </TouchableOpacity>
+              ),
+          )}
+        </ScrollView>
+
+        <Text style={styles.label}>Título</Text>
+        <TextInput
+          style={styles.input}
+          maxLength={30}
+          placeholder="Lembre-me de fazer"
+        />
+
+        <Text style={styles.label}>Detalhes</Text>
+        <TextInput
+          style={styles.inputArea}
+          maxLength={200}
+          multiline
+          placeholder="Detalhes da atividade que eu tenho que lembrar..."
+        />
+
+        <View style={styles.inLine}>
+          <View style={styles.inputInLine}>
+            <Switch
+              onValueChange={() => setDone(!done)}
+              value={done}
+              thumbColor={done ? '#00761B' : '#EE6B26'}
+            />
+            <Text style={styles.switchLabel}>Concluído</Text>
+          </View>
+          <TouchableOpacity>
+            <Text style={styles.removeLabel}>EXCLUIR</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+      <Footer isEditing />
+    </KeyboardAvoidingView>
+  );
+}
